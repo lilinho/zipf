ed205e93c474500ed176e2aea5d11fa5c5774f1b
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-10-22 13:26:46 +0200

    added plot. need improvement

--- processfile.py
+++ processfile.py
@@ -1,5 +1,6 @@
 import re
 from collections import Counter
+import matplotlib.pyplot as plt
 
 class ProcessFile:
 
@@ -7,12 +8,36 @@ class ProcessFile:
         self.file = open(name, 'r')
         self.string = self.file.read()
     def strip_string(self):
-        self.stripped_string = re.sub('[^A-Za-z0-9 ]+', ' ', self.string)
+        self.stripped_string = re.sub('[^A-Za-z0-9 ]+', ' ', self.string, re.UNICODE)
     def count_words(self):
         self.words = self.stripped_string.split()
         self.counted_words = Counter(self.words)
+        self.counted_words = dict(self.counted_words.most_common(20))
         print(self.counted_words)
+        return self.counted_words
+
+class Plotter:
+
+    def __init__(self, data):
+        self.words = []
+        self.counters = []
+        for word, count in data.items():
+            self.words.append(word)
+            self.counters.append(count)
+
+
+    def draw_plot(self):
+
+        fig = plt.figure()
+        pl = fig.add_subplot(111)
+        pl.plot(self.counters)
+        pl.set_xticklabels(self.words)
+
+        #plt.plot(self.words, self.counters)
+        #pl.show()
+        fig.show()
         
 f = ProcessFile("plik.txt")
 f.strip_string()
-f.count_words()
+plot = Plotter(f.count_words())
+plot.draw_plot()
