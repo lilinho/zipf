f0a31f663a93d6adf1ab7bdf3759cc7cf3bfe0ad
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-10-22 12:32:35 +0200

    word counter

--- processfile.py
+++ processfile.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 class ProcessFile:
 
@@ -6,8 +7,12 @@ class ProcessFile:
         self.file = open(name, 'r')
         self.string = self.file.read()
     def strip_string(self):
-        self.stripped_string = re.sub('[^A-Za-z0-9 ]+', '', self.string)
-        print(self.stripped_string)
-
+        self.stripped_string = re.sub('[^A-Za-z0-9 ]+', ' ', self.string)
+    def count_words(self):
+        self.words = self.stripped_string.split()
+        self.counted_words = Counter(self.words)
+        print(self.counted_words)
+        
 f = ProcessFile("plik.txt")
 f.strip_string()
+f.count_words()
