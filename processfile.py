51e4746509541dbf1a6dbe0b148ce6fb873fe00f
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2017-10-23 10:15:17 +0200

    newest

--- processfile.py
+++ processfile.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 import matplotlib.pyplot as plt
+import string
 
 class ProcessFile:
 
@@ -8,7 +9,9 @@ class ProcessFile:
         self.file = open(name, 'r')
         self.string = self.file.read()
     def strip_string(self):
-        self.stripped_string = re.sub('[^A-Za-z0-9 ]+', ' ', self.string, re.UNICODE)
+        #self.stripped_string = re.sub('[^A-Za-z0-9- ]+', ' ', self.string, re.UNICODE)
+        #self.stripped_string = re.sub('[-', ' ', self.string, re.UNICODE)
+        self.stripped_string = self.string.translate(string.punctuation)
     def count_words(self):
         self.words = self.stripped_string.split()
         self.counted_words = Counter(self.words)
@@ -25,21 +28,24 @@ class Plotter:
             self.words.append(word)
             self.counters.append(count)
 
-
+    def func(self, x, i):
+        return x/i
+    
     def draw_plot(self):
 
+        xticks = self.words
         y2 = []
-        for i in range(len(self.counters)):
-            y2.append(self.counters[i]/(i+1))
+        for i in range(100):
+            y2.append(self.counters[0]/(i+1))
 
         fig = plt.figure()
         pl = fig.add_subplot(111)
         pl.plot(self.counters, 'o')
         pl.set_xticklabels(self.words)
-        pl.plot(y2)
+        #pl.plot(y2)
+        print(self.words)
+        fig.savefig("out.png")
 
-        #plt.plot(self.words, self.counters)
-        #pl.show()
         fig.show()
         
 f = ProcessFile("plik.txt")
